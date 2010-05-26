ad950395b6a2831aae0feb5cc71eb71d6a5cde97
Joel Nimety <jnimety@continuity.net>
2010-05-26 16:59:00 -0400

    user our button style
    Signed-off-by: Joel Nimety <jnimety@continuity.net>

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -3,11 +3,11 @@ module FriendshipsHelper
   def friendship_control_links(friendship)
     case friendship.friendship_status_id
       when FriendshipStatus[:pending].id
-        "#{(link_to(:accept.l, accept_user_friendship_path(friendship.user, friendship), :method => :put, :class => 'button positive') unless friendship.initiator?)} #{link_to(:deny.l, deny_user_friendship_path(friendship.user, friendship), :method => :put, :class => 'button negative')}"
+        "#{(link_to("<span>" + :accept.l + "</span>", accept_user_friendship_path(friendship.user, friendship), :method => :put, :class => 'button rect_button small_green_buttton') unless friendship.initiator?)} #{link_to(:deny.l, deny_user_friendship_path(friendship.user, friendship), :method => :put, :class => 'button negative')}"
       when FriendshipStatus[:accepted].id
-        "#{link_to(:remove_this_friend.l, deny_user_friendship_path(friendship.user, friendship), :method => :put, :class => 'button negative')}"
+        "#{link_to("<span>" + :remove_this_friend.l + "</span>", deny_user_friendship_path(friendship.user, friendship), :method => :put, :class => 'button rect_button')}"
       when FriendshipStatus[:denied].id
-    		"#{link_to(:accept_this_request.l, accept_user_friendship_path(friendship.user, friendship), :method => :put, :class => 'button positive')}"
+    		"#{link_to("<span>" + :accept_this_request.l + "</span>", accept_user_friendship_path(friendship.user, friendship), :method => :put, :class => 'button rect_button small_green_button')}"
     end
   end
 
