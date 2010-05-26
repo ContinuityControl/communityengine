0863160f327536c2a43ea4ac5c35d164ef7d8ef0
Joel Nimety <jnimety@continuity.net>
2010-05-26 17:00:19 -0400

    user our button style
    Signed-off-by: Joel Nimety <jnimety@continuity.net>

--- app/helpers/base_helper.rb
+++ app/helpers/base_helper.rb
@@ -184,13 +184,13 @@ module BaseHelper
 
   def add_friend_link(user = nil)
 		html = "<span class='friend_request' id='friend_request_#{user.id}'>"
-    html += link_to_remote "<span>" + :request_friendship.l + "</span>",
+    html += link_to_remote :request_friendship.l,
 				{:update => "friend_request_#{user.id}",
 					:loading => "$$('span#friend_request_#{user.id} span.spinner')[0].show(); $$('span#friend_request_#{user.id} a.add_friend_btn')[0].hide()", 
 					:complete => visual_effect(:highlight, "friend_request_#{user.id}", :duration => 1),
           500 => "alert('"+:sorry_there_was_an_error_requesting_friendship.l.gsub(/'/, "\\\\'")+"')",
 					:url => hash_for_user_friendships_url(:user_id => current_user.id, :friend_id => user.id), 
-					:method => :post }, {:class => "add_friend button rect_button small_green_button"}
+					:method => :post }, {:class => "add_friend button"}
 		html +=	"<span style='display:none;' class='spinner'>"
 		html += image_tag 'spinner.gif', :plugin => "community_engine"
 		html += :requesting_friendship.l+" ...</span></span>"
