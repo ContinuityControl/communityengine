ee243d45013bdc031134a800cdc37b2b6f50f250
Joel Nimety <jnimety@continuity.net>
2011-01-15 21:52:52 -0500

    specify data formats rails 3 style
    Signed-off-by: Joel Nimety <jnimety@continuity.net>

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,2 +1,4 @@
-ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.update :line_grapher => '%Y%m%dT%H:%M:%S'
-ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.update :line_graph => '%m/%d'
+Time::DATE_FORMATS[:line_grapher] = lambda { |date| date.strftime("%Y%m%dT%H:%M:%S") }
+Date::DATE_FORMATS[:line_grapher] = lambda { |date| date.strftime("%Y%m%dT%H:%M:%S") }
+Time::DATE_FORMATS[:line_graph] = lambda { |date| date.strftime("%m/%d") }
+Date::DATE_FORMATS[:line_graph] = lambda { |date| date.strftime("%m/%d") }
