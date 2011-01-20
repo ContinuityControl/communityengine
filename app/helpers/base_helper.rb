c4ca13ac01e87db620f93f973a6fd059b42951cc
Joel Nimety <jnimety@continuity.net>
2011-01-19 22:47:51 -0500

    _remote methods are deprecated, use :remote => true instead
    Signed-off-by: Joel Nimety <jnimety@continuity.net>

--- app/helpers/base_helper.rb
+++ app/helpers/base_helper.rb
@@ -183,8 +183,8 @@ module BaseHelper
 
   def add_friend_link(user = nil)
 		html = "<span class='friend_request' id='friend_request_#{user.id}'>"
-    html += link_to_remote :request_friendship.l,
-				{:update => "friend_request_#{user.id}",
+    html += link_to :request_friendship.l,
+				{:remote => true, :update => "friend_request_#{user.id}",
 					:loading => "$$('span#friend_request_#{user.id} span.spinner')[0].show(); $$('span#friend_request_#{user.id} a.add_friend_btn')[0].hide()", 
 					:complete => visual_effect(:highlight, "friend_request_#{user.id}", :duration => 1),
           500 => "alert('"+:sorry_there_was_an_error_requesting_friendship.l.gsub(/'/, "\\\\'")+"')",
