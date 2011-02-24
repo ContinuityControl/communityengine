3c3a53f01667dee3e2875a3e444984398917dc0c
Joel Nimety <jnimety@continuity.net>
2011-02-24 12:07:59 -0500

    DEPRECATION WARNING: RAILS_ROOT is deprecated. Please use ::Rails.root.to_s
    Signed-off-by: Joel Nimety <jnimety@continuity.net>

--- config/initializers/globalite.rb
+++ config/initializers/globalite.rb
@@ -1,7 +1,7 @@
 
 require 'globalite_extensions'
-I18n.load_path += Dir[ (File.join(RAILS_ROOT, "vendor", "plugins", "community_engine", "lang", "ui", '*.{rb,yml}')) ] - I18n.load_path
-I18n.load_path += Dir[ (File.join(RAILS_ROOT, "lang", "ui", '*.{rb,yml}')) ] - I18n.load_path
+I18n.load_path += Dir[ (Rails.root.join("vendor", "plugins", "community_engine", "lang", "ui", '*.{rb,yml}').to_s) ] - I18n.load_path
+I18n.load_path += Dir[ (Rails.root.join("lang", "ui", '*.{rb,yml}').to_s) ] - I18n.load_path
 # I18n.load_path += Dir[ (File.join(RAILS_ROOT, "lang", "rails", '*.{rb,yml}')) ]
 I18n.default_locale = "en"
 I18n.reload!
